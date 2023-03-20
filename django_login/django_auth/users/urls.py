ab07f160ff370ccc713cd93ce729b5a95455db42
Cyril Kevin <76154339+cack195@users.noreply.github.com>
2023-03-20 14:02:06 +0530

    Delete django_login/django_auth directory

--- django_login/django_auth/users/urls.py
+++
@@ -1,8 +0,0 @@
-from django import views
-from django.urls import path
-from .views import home, SignUp
-
-urlpatterns = [
-	path('home/', home, name='home'),
-	path('signup/', SignUp.as_view(), name='signup'),
-]
